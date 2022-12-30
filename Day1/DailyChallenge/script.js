c35376f58aa39c1e76b421e0f49783c0036bd908
yanbert <yanbert@gmail.com>
2022-12-30 01:48:35 +0000

    Voir mes commentaires dans le code

--- Day1/DailyChallenge/script.js
+++ Day1/DailyChallenge/script.js
@@ -11,6 +11,12 @@ console.log(fruits);
 fruits.splice(0,1); // removing Apples from array.
 
 // reversing of array
+/******************************************************
+Correcteur: Yannick GOUGUIA
+
+Pas d'espace autour des =
+
+*****************************************************/
 let changeValue;
 changeValue = fruits[0];
 fruits[0]=fruits[2];
@@ -19,6 +25,12 @@ console.log(fruits);
 
 // Exercice  2
 
+/******************************************************
+Correcteur: Yannick GOUGUIA
+
+Revoir cet exercice, mauvaise reponse
+
+*****************************************************/
 const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
 console.log(moreFruits[1][1][0]);
